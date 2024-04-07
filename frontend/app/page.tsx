1d9fff3e11a374417efb421c8b4ae30c44882f90
rKAmindo <randykamindo@gmail.com>
2024-04-07 10:11:41 -0400

    Create Next.js project and configure user authentication
    issue #4

---
+++ frontend/app/page.tsx
@@ -0,0 +1,11 @@
+import ProfileClient from "./components/ProfileClient";
+
+export default function Home() {
+  return (
+    <div>
+      <a href="/api/auth/login">Login</a>
+      <a href="/api/auth/logout">Logout</a>
+      <ProfileClient />
+    </div>
+  );
+}
