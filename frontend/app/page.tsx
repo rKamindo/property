9cc1521ec70a41ad6939424a7813acbd280e4e59
rKAmindo <randykamindo@gmail.com>
2024-04-22 12:31:14 -0400

    Add hero page and navigation sidebar with header
    Add hero page with headline, description, and CTA. Implement navbar with basic links. Implement ability to log in on hero page, and logout in main page.
    
    issue #12

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,21 @@
-import ProfileClient from "./components/ProfileClient";
+"use client";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import Hero from "./components/Hero";
+import SidebarWithHeader from "./components/SideBarWithHeader";
+import { Center, Spinner } from "@chakra-ui/react";
 
-export default function Home() {
-  return (
-    <div>
-      <a href="/api/auth/login">Login</a>
-      <a href="/api/auth/logout">Logout</a>
-      <ProfileClient />
-    </div>
-  );
+export default function Index() {
+  const { user, isLoading } = useUser();
+  if (isLoading) {
+    return (
+      <Center h="100vh" w="100vw">
+        <Spinner size="xl" />
+      </Center>
+    );
+  }
+  if (user) {
+    return <SidebarWithHeader />;
+  } else {
+    return <Hero />;
+  }
 }
